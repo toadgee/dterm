ec5bdfb22f040d098e8f88646ae16999fc30ec36
OrangeRaven <apfelsinenhain@gmx.de>
2014-11-02 13:51:19 +0100

    add small convenience to `setaxtrused.sh` ... makes copy&paste to the Terminal easier

--- _ideas/setaxtrusted.sh
+++ _ideas/setaxtrusted.sh
@@ -1,5 +1,7 @@
 # to add on 10.9:
 sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "INSERT or REPLACE INTO access values ('kTCCServiceAccessibility', 'com.company.app', 0, 1, 0, NULL);"
+    # for DTerm:
+    sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "INSERT or REPLACE INTO access values ('kTCCServiceAccessibility', 'net.decimus.dterm', 0, 1, 0, NULL);"
 
 # to remove
 sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "delete from access where client='com.company.app';"
